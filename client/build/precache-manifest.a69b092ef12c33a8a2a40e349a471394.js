0a362264c846e7636c0f76b756bf2fb6894071f1
Yantzin Cabrera <yantzincabrera@gmail.com>
2019-11-08 20:42:25 -0800

    moved gitignore file

---
+++ client/build/precache-manifest.a69b092ef12c33a8a2a40e349a471394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06aaf9b52e1874690a47b2a3b8debf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ac0e8017f9afff96f4",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c21e3795e7f9e89ce61e",
+    "url": "/static/js/2.e8a26374.chunk.js"
+  },
+  {
+    "revision": "33ac0e8017f9afff96f4",
+    "url": "/static/js/main.f068f86b.chunk.js"
+  },
+  {
+    "revision": "8add5019cb9658630475",
+    "url": "/static/js/runtime-main.a9c50454.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ client/src/components/NoMatch/NoMatch.components.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Jumbotron} from 'reactstrap';
+export const NoMatch = props => {
+   
+    return(
+
+    );
+}
