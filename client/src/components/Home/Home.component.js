a36eb64ce19e91101f68c040db4e6bc1879d4999
Yantzin Cabrera <yantzincabrera@gmail.com>
2019-11-08 21:08:10 -0800

    added Home component file

---
+++ client/src/components/Home/Home.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+export const Home = props => {
+    return (
+
+    );
+};
--- client/src/components/NoMatch/NoMatch.components.js
+++ client/src/components/NoMatch/NoMatch.components.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Jumbotron} from 'reactstrap';
 export const NoMatch = props => {
-   
-    return(
+  return(
 
-    );
-}
+  );
+};
+    
+
+   
