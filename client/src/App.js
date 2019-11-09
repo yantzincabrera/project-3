6071fa14eeb5bb65170d16d811282e7b336c9199
Yantzin Cabrera <yantzincabrera@gmail.com>
2019-11-08 22:16:19 -0800

    worked on app.js code

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router , Switch, Route} from 'react-router-dom';
-import {NoMatch} from './NoMatch';
+import { Home } from './components/Home/Home.component';
+import {NoMatch} from './components/NoMatch';
 
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        
+      </Switch>
+    </Router>
    
   );
 }
