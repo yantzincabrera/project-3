42c58b4de03c6f77877d7ac028e02c37a92ebde6
Ruth Vazquez <rthvazquez@gmail.com>
2019-11-25 21:21:53 -0800

    working on the userpost file

--- client/src/components/UserPost/UserPost.js
+++ client/src/components/UserPost/UserPost.js
@@ -6,7 +6,9 @@ class EssayForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'What would you like to express?',
+      value: 'this is the only thing working so far, it shows as a tribute',
+      firstName: '',
+      lastName: ''
 
     };
 
@@ -15,7 +17,7 @@ class EssayForm extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit = (event) => {
@@ -24,6 +26,7 @@ class EssayForm extends React.Component {
       showPost: true,
     });
     console.log(this.state.value);
+    console.log(this.state.name);
   }
 
   clearForm = () => {
@@ -34,13 +37,40 @@ class EssayForm extends React.Component {
     return (
 //first and last name
    
-      <form onSubmit={this.handleSubmit}>
+    <form onSubmit={this.handleSubmit}>
+        <div className="form-row">
+        <div className="col">
+          <input 
+          type="text"
+          name="firstName" 
+          //value={this.state.firstName}
+          class="form-control" 
+          placeholder="First name" 
+          onChange={this.handleChange}/>
+        </div>
+        <div className="col">
+          <input 
+          type="text"
+          name="lastName" 
+          class="form-control" 
+          placeholder="Last name" 
+          onChange={this.handleChange}/>
+        </div>
+        </div>
           <textarea class="form-control" id="validationTextarea" value={this.state.value} onChange={this.handleChange}></textarea>
         
         <div className="uploadButton"><input type="file" name="imgUpload"/></div>  
-        <div  className="submitButton"><button variant="contained" color="primary" className="submitButton" type="button" value="Submit" onClick={this.handleSubmit}>Post
-          </button></div> 
-          {this.state.showPost && <div className="newPost">{this.state.value}</div>}
+        <div className="submitButton">
+          <button 
+          variant="contained" 
+          class="btn btn-primary"  
+          type="button" 
+          value="Submit" 
+          onClick={this.handleSubmit}>Post</button>
+        </div> 
+        <div className="tributeLabel"> Tributes
+            {this.state.showPost && <div className="newPost">{this.state.value}</div>}
+        </div>
       </form>
      
     );
