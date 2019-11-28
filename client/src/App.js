f3e6237a8f79861f0228acc91c7fb12a213f4728
Ruth Vazquez <rthvazquez@gmail.com>
2019-11-27 18:38:28 -0800

    added datePassed so user can input the date for when their loved one passed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router , Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home.component';
 import {NoMatch} from './components/NoMatch/NoMatch.components';
 
-import Navbar from './components/NavBar/Navbar.js';
+import Navbar from './components/NavBar/navbar.js';
 import {Login} from './components/Login.js';
 import {Signup} from './components/Sign-Up/signup';
 import {Help} from "./components/Help/help";
@@ -19,7 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/Memorial" component={Memorial} />
+        
         <Route path="/signup" component={Signup} /> 
         <Route path="/help" component={Help} />
         <Route component={NoMatch} />
--- client/src/components/UserPost/UserPost.js
+++ client/src/components/UserPost/UserPost.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./UserPost.css";
+//import { EditorModeComment } from 'material-ui/svg-icons';
+//import { DatePicker } from 'react-datepicker';
+import datePassed from '../DatePassed/datePassed.js';
+
+
 
 
 class EssayForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'this is the only thing working so far, it shows as a tribute',
-      firstName: '',
-      lastName: ''
-
+      value: 'Once upon a time .......',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -17,7 +19,10 @@ class EssayForm extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ 
+      value: event.target.value,
+   });
+    
   }
 
   handleSubmit = (event) => {
@@ -35,11 +40,12 @@ class EssayForm extends React.Component {
 
   render() {
     return (
-//first and last name
-   
-    <form onSubmit={this.handleSubmit}>
-        <div className="form-row">
-        <div className="col">
+    <form onSubmit={this.handleSubmit}> <label>Give us infromation about your loved one</label>
+      <div className="datePassed"> When did they pass away?
+     <datePassed />
+      </div>
+      <div className="form-row"> What was their name?
+      <div className="col">
           <input 
           type="text"
           name="firstName" 
@@ -47,30 +53,30 @@ class EssayForm extends React.Component {
           class="form-control" 
           placeholder="First name" 
           onChange={this.handleChange}/>
-        </div>
-        <div className="col">
+      </div>
+      <div className="col"> 
           <input 
           type="text"
           name="lastName" 
           class="form-control" 
           placeholder="Last name" 
           onChange={this.handleChange}/>
-        </div>
-        </div>
+      </div>
+      </div>Tell us about them, like your favorite memory.
           <textarea class="form-control" id="validationTextarea" value={this.state.value} onChange={this.handleChange}></textarea>
         
-        <div className="uploadButton"><input type="file" name="imgUpload"/></div>  
-        <div className="submitButton">
+      <div className="uploadButton">Share a picture of your loved one<input type="file" name="imgUpload"/></div>  
+      <div className="submitButton">
           <button 
           variant="contained" 
           class="btn btn-primary"  
           type="button" 
           value="Submit" 
           onClick={this.handleSubmit}>Post</button>
-        </div> 
-        <div className="tributeLabel"> Tributes
+      </div> 
+      <div className="tributeLabel"> Tributes
             {this.state.showPost && <div className="newPost">{this.state.value}</div>}
-        </div>
+      </div>
       </form>
      
     );
